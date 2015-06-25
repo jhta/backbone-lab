b132de07f340fd1e1720151b05034770801d4761
jhta <jeisonhs93@gmail.com>
2015-06-25 00:52:24 -0500

    gulp server

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,60 @@ var gulp       = require("gulp"),
     gutil      = require("gulp-util"),
     nib        = require("nib");
 
-gulp.task("default", function(){
-  console.log("hello world!!");
+
+
+/**
+ * STYLUS TASK
+ * ************
+ * pre-compile stylus to CSS
+ */
+
+gulp.task('stylus', function() {
+  gulp.src('dev/styl/main.styl')
+    .pipe(stylus({
+      compress: true,
+      use: nib()
+    }))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('dist/css'))
 });
+
+
+/**
+ * COPY TASK
+ * ************
+ * Copy index to 'dist' folder
+ */
+gulp.task("copy", function() {
+  gulp.src('dev/index.html')
+    .pipe(gulp.dest("dist"))
+});
+
+
+/**
+ * SERVER TASK
+ * ************
+ * Run server
+ * default port: 8000
+ */
+
+gulp.task('server', function() {
+  gulp.src('dist')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: false,
+      fallback : 'index.html'
+  }));
+});
+
+
+
+gulp.task('watch', function() {
+  gulp.watch('dev/styl/**/*.styl', ['stylus']);
+  gulp.watch('dev/**/*.html', ['copy']);
+})
+
+
+gulp.task('build', ['stylus', 'copy']);
+gulp.task('dev', ['build', 'server', 'watch']);
