3e2acdf5547c593782e9e07d6d5a6bdee042bbe8
jhta <jeisonhs93@gmail.com>
2015-06-28 22:32:41 -0500

    UI finished

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,9 +1,10 @@
 const $ = require("jquery")(window),
       _ = require("underscore"),
       Backbone = require("backbone");
+
 Backbone.$ = $;
 
-const Router = require("./router");
-const router = new Router();
-
-Backbone.history.start();
+// const Router = require("./router");
+// const router = new Router();
+//
+// Backbone.history.start();
